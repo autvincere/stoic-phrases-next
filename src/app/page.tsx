e4f6694a5fbf079e6f872acd832820d1ab868d95
Eduardo <epalma.externo@coopeuch.cl>
2025-02-10 15:01:26 -0300

    fix: Update API fetch URL to use environment variable for improved flexibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 import Card from "../app/components/card/Card";
 
 const HomePage = async () => {
-  let phrase
-try {
-  const data = await fetch("http://localhost:3000/api/phrases");
-  
-  if (!data.ok) {
-    throw new Error(`HTTP error! status: ${data.status}`);
+  let phrase;
+  try {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/phrases`);
+
+    if (!data.ok) {
+      console.log(" error data: ", data);
+      throw new Error(`HTTP error! status: ${data.status}`);
+    }
+
+    phrase = await data.json();
+    console.log("phrase: ", phrase);
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : "An error occurred");
   }
-  
-  phrase = await data.json();
-  console.log('phrase: ', phrase );
-} catch (error) {
-  console.log(error instanceof Error ? error.message : 'An error occurred');
-}
 
   //  console.log('phrase: ', phrase );
 
