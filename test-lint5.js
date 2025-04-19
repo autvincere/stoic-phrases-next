1d6253f519b1e15138399d379a1eac80d32a0c75
Eduardo <epalma.externo@coopeuch.cl>
2025-04-19 01:06:53 -0400

    Fifth test with linting errors

---
+++ test-lint5.js
@@ -0,0 +1,9 @@
+// Este archivo tiene errores de lint intencionales
+function test5() {
+  var v = 50;
+  if (v == 50) {
+    console.log('Test 5');
+  }
+
+  const unusedVar5 = 'This variable is unused';
+}
