83105dd3ab1fc5a8047e0ae90730a01c6e9716a2
Eduardo <epalma.externo@coopeuch.cl>
2025-04-19 01:03:32 -0400

    Fourth test with linting errors

---
+++ test-lint4.js
@@ -0,0 +1,9 @@
+// Este archivo tiene errores de lint intencionales
+function test4() {
+  var w = 40;
+  if (w == 40) {
+    console.log('Test 4');
+  }
+
+  const unusedVar4 = 'This variable is unused';
+}
